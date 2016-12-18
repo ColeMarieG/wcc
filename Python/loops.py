244cec787a6385843b950c74d1a0963e5982a218
ColeMarieG <cole.gailus@gmail.com>
2016-12-17 23:30:13 -0500

    For and while loop examples

---
+++ Python/loops.py
@@ -0,0 +1,36 @@
+# # While Loop Structure:
+# while boolean expression:
+#     statement(s) to execute
+
+# counter = 1
+# while counter <= 10:
+#     print '#' * counter
+#     counter = counter + 1
+
+
+# # For Loop Structure:
+# for iterator in sequence:
+#     statement(s) to execute
+
+# # First, define the sequence we'll loop through
+# numbers = range(0,10)
+# # Used Built-in Python function range() to produce a list: [0,1,2,3,4,5,6,7,8,9]
+#
+# for counter in numbers:
+#     print '#' * counter
+
+## Non-numeric loops
+# greeting = 'Hello World'
+# for letter in greeting:
+#     print letter
+
+# continents = ['Africa', 'Antarctica', 'Asia', 'Europe', 'North America', 'Australia', 'South America']
+# for continent in continents:
+#     print continent + ' is a continent.'
+
+# def print_vowels():
+#     vowels = ['a', 'e', 'i', 'o', 'u']
+#     # vowels = 'aeiou'
+#     for item in vowels:
+#         print item
+# print_vowels()
